e75e2fca65a58a8568baa4f8a6989054c4619101
masato <ponsima1140@gmail.com>
2019-11-11 12:06:44 +0900

    [update] ボタンサイズ変更

--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -1,26 +1,27 @@
 <template>
+  <!-- <v-bottom-navigation grow fixed> -->
   <v-bottom-navigation grow fixed>
-    <v-btn to="/" height="btn_size" nuxt>
+    <v-btn to="/" :height="btn_size" nuxt>
       <div>HOME</div>
       <v-icon :size="logo_size">home</v-icon>
     </v-btn>
 
-    <v-btn to="profile" height="btn_size">
+    <v-btn to="profile" :height="btn_size">
       <div>Profile</div>
       <v-icon :size="logo_size">account_circle</v-icon>
     </v-btn>
 
-    <v-btn to="skills" height="btn_size">
+    <v-btn to="skills" :height="btn_size">
       <div>Skill</div>
       <v-icon :size="logo_size">computer</v-icon>
     </v-btn>
 
-    <v-btn to="products" height="btn_size">
+    <v-btn to="products" :height="btn_size">
       <div>Product</div>
       <v-icon :size="logo_size">web</v-icon>
     </v-btn>
 
-    <v-btn to="account" height="btn_size">
+    <v-btn to="account" :height="btn_size">
       <div>Account</div>
       <v-icon :size="logo_size">contact_mail</v-icon>
     </v-btn>
@@ -33,7 +34,7 @@ export default {
     return {
       bottomNav: "recent",
       activeBtn: 1,
-      btn_size: "56px",
+      btn_size: "65px",
       logo_size: "40px"
     };
   }
