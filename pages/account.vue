5e55f3019554e4279387cf0e229bd73983f50bd6
masato <ponsima1140@gmail.com>
2019-12-30 16:38:08 +0900

    [change] cssをscssに変更

--- pages/account.vue
+++ pages/account.vue
@@ -9,17 +9,25 @@
           <div class="contact-list">
             <ul>
               <li>
-                <a href="https://github.com/ponsima" class="display-1 font-weight-regular">GitHub</a>
+                <div class="firstImg">
+                  <img src="~/assets/logo/github-icon.svg" alt />
+                  <a
+                    href="https://github.com/ponsima"
+                    class="display-1 font-weight-regular huga"
+                  >GitHub</a>
+                </div>
               </li>
 
               <li>
-                <a href="https://qiita.com/ponsima" class="display-1 font-weight-regular">Qiita</a>
+                <img src="~/assets/logo/facebook.svg" alt />
+                <a href="https://qiita.com/ponsima" class="display-1 font-weight-regular">facebook</a>
               </li>
 
               <li>
+                <img src="~/assets/logo/wantedly_mark.png" alt="wantedlyIcon" />
                 <a
                   href="https://www.wantedly.com/users/103275557"
-                  class="display-1 font-weight-regular"
+                  class="display-1 font-weight-regular huga"
                 >Wantedly</a>
               </li>
               <p class="text">
@@ -38,56 +46,90 @@
   </v-app>
 </template>
 
-<style scoped>
+
+<style scoped lang="scss" >
 * {
   color: #333;
 }
-section .wrapper {
-  padding: 80px 10px;
+
+section {
+  .wrapper {
+    padding: 80px 10px;
+  }
+
+  .title-block {
+    text-align: center;
+    margin-bottom: 30px;
+  }
+
+  img {
+    display: inline;
+  }
+
+  .text {
+    margin-top: 3em;
+    line-height: 1.5em;
+  }
+
+  .mail {
+    padding-left: 0.5em;
+    font-size: 1.5em;
+  }
+
+  li {
+    margin: 10px 0 10px 0;
+    text-align: center;
+  }
+
+  .firstImg {
+    padding-right: 40px;
+  }
+
+  .wrapper {
+    .contact-wrapper {
+      .contact-list {
+        ul {
+          a {
+            text-decoration: none;
+            text-align: center;
+          }
+        }
+      }
+    }
+  }
 }
-section .title-block {
-  text-align: center;
-  margin-bottom: 30px;
+
+#contact {
+  .contact-list {
+    display: inline;
+    padding: 10px 0;
+  }
+
+  .contact-list ul {
+    width: 45%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-self: center;
+    font-size: 18px;
+  }
+
+  .contact-list li a {
+    text-decoration: none;
+    color: #0095eb;
+  }
+
+  .contact-list li img {
+    width: 50px;
+  }
+
+  .mail {
+    color: #0095eb;
+  }
 }
-section .text {
-  margin-top: 3em;
-  line-height: 1.5em;
-}
-section .mail {
-  padding-left: 0.5em;
-  font-size: 1.5em;
-}
-section .wrapper > .contact-wrapper > .contact-list > ul > a {
-  text-decoration: none;
-  text-align: center;
-}
-#contact .contact-list {
-  padding: 10px 0;
-}
-#contact .contact-list ul {
-  width: 45%;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-self: center;
-  font-size: 18px;
-}
-#contact .contact-list li {
-  text-align: center;
-  line-height: 3.5em;
-}
-#contact .contact-list li a {
-  color: #0095eb;
-}
-#contact .contact-list li img {
-  width: 60px;
-  margin: 0 auto 5px;
-}
-.mail {
-  color: #0095eb;
-}
+
 @media screen and (max-width: 767px) {
   section .wrapper {
     padding: 80px 10px;
@@ -109,11 +151,9 @@ section .wrapper > .contact-wrapper > .contact-list > ul > a {
   }
   #contact .contact-list li {
     flex-basis: 30%;
-    text-align: center;
-    margin: 0 auto;
   }
   #contact .contact-list li img {
-    width: 30px;
+    width: 50px;
   }
   #contact .contact-list-desc {
     font-size: 12px;
